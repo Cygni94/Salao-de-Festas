e902768cb950f2a65790f4111f4673c75c07c2b5
Cristiano Coelho <ccoelho.me@gmail.com>
2018-05-15 17:24:56 -0300

    matrizes declaradas

--- main.c
+++ main.c
@@ -3,6 +3,10 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    colunas = 10;//temporariamente será usada uma quantidade fixa de colunas, depois especificar quantidade ideal.
+    int cliente[4][colunas], funcionario[5][colunas], fornecedor[3][colunas], festa[5][colunas], contrato[6][colunas];
+
+    for (i=0; i<)
+
     return 0;
 }
