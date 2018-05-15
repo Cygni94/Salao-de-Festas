a6a939b8f7abfe391d69b6d70a8aeb8b730c0990
Cristiano Coelho <ccoelho.me@gmail.com>
2018-05-15 17:57:06 -0300

    testing cliente

--- main.c
+++ main.c
@@ -3,10 +3,24 @@
 
 int main()
 {
-    colunas = 10;//temporariamente será usada uma quantidade fixa de colunas, depois especificar quantidade ideal.
+    int colunas = 10;//temporariamente será usada uma quantidade fixa de colunas, depois especificar quantidade ideal para cada campo.
     int cliente[4][colunas], funcionario[5][colunas], fornecedor[3][colunas], festa[5][colunas], contrato[6][colunas];
+    char clienteLabel[] = {'Nome', 'Endereco', 'Telefone', 'Data de Nascimento'};
+    int i, j, opcao;
 
-    for (i=0; i<)
+    switch(opcao) {
 
+case 1:
+    printf("Cliente")
+
+    for (i=0; i<colunas; i++) {
+            printf("Codigo: %d\n", i);
+        for (j=0; j<4; j++) {
+            printf("%s", clienteLabel[j]);
+            scanf("%s", cliente[i][j]);
+        }
+    }
+
+    }
     return 0;
 }
