da141b4ad4ef74220d7ac790226efb3f11896345
Cristiano Coelho <ccoelho.me@gmail.com>
2018-05-30 17:35:25 -0300

    menu switch e funcoes em andamento

--- main.c
+++ main.c
@@ -2,12 +2,29 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct typedef Cliente {
+typedef struct Cliente {
     int codigo, telefone, dataNasc;
     char nome[50], endereco[100];
 } Cliente;
 
 int main() {
-    Cliente cliente;
+    int opcao;
+
+    printf("####SALAO DE FESTAS####\n#####PATATI PATATA#####\n\n");
+    //Menu principal
+    printf("\nMenu:\n1-Cadastrar cliente\n2-Cadastrar funcionario\n3-Cadastrar fornecedor\n4-Cadastrar uma festa\n");
+    printf("5-Fazer um orcamento\n6-Atualizar status de contrato\n7-Consultas de informacoes cadastrais\n8-Relatorio de festas\n");
+    printf("9-Relatorios de festas\n");
+    //Submenu 7 - Informacoes cadastrais
+    //Submenu 9 - Relatorios de festas
+    scanf("%d", &opcao);
+    switch (opcao) {
+case 1:
+
+    break;
+    }
+
+
+
     return 0;
 }
